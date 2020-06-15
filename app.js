b6e51ff3bc9ba22425ea5a545b42a3d73b3391b0
muhammadafzalsiddiqui <49964804+muhammadafzalsiddiqui@users.noreply.github.com>
2020-06-15 23:39:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,310 @@
+// ===== chap #1 task 1======
+// alert("wellcome to my website");
+
+
+// ===== chap #1 task 2======
+// alert("Error! Please enter a valid password");
+
+
+// ===== chap #1 task 3======
+// alert("wellcome to JS Land.. \n Happy Coding");
+
+
+// ===== chap #1 task 5======
+// console.log("Hello..I can run Js through my web browser console")
+
+
+
+// ===== chap #1 task 6======
+// alert("my html file")
+
+// ===== chap #1 task 7 cheack in index file======
+
+
+
+
+// ===== chap #2 task 1======
+// var userName;
+
+
+// ===== chap #2 task 2======
+// var myName = ("muhammad afzal siddiqui");
+// alert(myName);
+
+
+// ===== chap #2 task 3======
+// var massage = ("Hello world");
+// alert(massage);
+
+
+// ===== chap #2 task 4======
+
+// var studentName = ("afzal");
+// alert(studentName);
+// var studentAge = ("15 Years Old");
+// alert(studentAge);
+// var studentEducation = ("metric");
+// alert(studentEducation);
+
+
+// ===== chap #2 task 5======
+// var pizza = ("PIZZA \nPIZZ \nPIZ \nPI \nP");
+// alert(pizza);
+
+
+// ===== chap #2 task 7======
+// var email =("my Email address is ");
+// var address = ("khanafzal155@gmail.com")
+// var eaddress =(email+address);
+// alert(eaddress);
+
+
+// ===== chap #2 task 6====== 
+// var book =("i am try to learn")
+// var books =("A  smart way to learn js")
+// var c =(book+books)
+// alert(c);
+
+
+// ===== chap #2 task 8=====
+// var name = ("yah! i can write HTML content through java Script");
+// document.write(name);
+
+
+
+
+//===== chap #3 task 1=====
+// var age = ("i am 15 year old");
+// alert(age);
+
+
+//===== chap #3 task 2=====
+// var time =("have visited this site 15 times");
+// alert(time);
+
+
+//===== chap #3 task 3=====
+// var birthYear =("my birth year is 1990");
+// document.write (birthYear);
+
+
+//===== chap #3 task 4=====
+// var cloth =("John Doe ordered 5 T-shirt(s) on XYZ Clothing store");
+// document.write(cloth);
+
+
+
+//===== chap #4 task 1=====
+// var name new newNumber
+
+
+
+//===== chap #4 task 2=====
+// var rule =("A heading stating “Rules for naming JS variables <br>  <br> Variable names can only contain number, $, namefirst <br> Variables must begin with a ______, ______ or _____. For example $name, _name or name")
+// document.write(rule);
+
+
+//===== chap #5 task1 =====
+
+// var numOne=(8);
+// var numTwo=(5);
+// document.write(numOne+numTwo);
+
+
+
+//===== chap #5 task2 =====
+
+// var numOne=(8);
+// var numTwo=(5);
+// document.write(numOne-numTwo);
+
+
+
+//===== chap #5 task2 =====
+
+// var numOne=(8);
+// var numTwo=(5);
+// document.write(numOne*numTwo);
+
+
+
+//===== chap #5 task2 =====
+
+// var numOne=(8);
+// var numTwo=(5);
+// document.write(numOne%numTwo);
+
+
+//===== chap #5 task3 =====
+// var movieTicket =("total price of 5 ticket is")
+// var movies =(5*300);
+// document.write(movieTicket+movies);
+
+//===== chap #5 task4 =====
+// var a =("4*1=");
+// var b = (4*1);
+// document.write(a+b);
+
+// var a =("4*2=");
+// var b = (4*2);
+// document.write(a+b);
+// var a =("4*3=");
+// var b = (4*3 );
+// document.write(a+b);
+// var a =("4*4=");
+// var b = (4*4 );
+// document.write(a+b);
+// var a =("4*5=");
+// var b = (4*5 );
+// document.write(a+b);
+// var a =("4*6=");
+// var b = (4*6 );
+// document.write(a+b);
+// var a =("4*7=");
+// var b = (4*7 );
+// document.write(a+b);
+// var a =("4*8=");
+// var b = (4*8 );
+// document.write(a+b);
+// var a =("4*9=");
+// var b = (4*9 );
+// document.write(a+b);
+// var a =("4*10=");
+// var b = (4*10 );
+// document.write(a+b);
+
+
+
+
+//===== chap #5 task5 =====
+// var tempreture =("25 C is ")
+// var c = (70-32*5/9);
+// document.write(tempreture+c);
+// var tempreture =("70 F is ")
+// var c = (25-32*5/9);
+// document.write(tempreture+c);
+
+
+//===== chap #5 task6 =====
+// var sc=("Shoping Card <br>");
+// var pi =("price of item 1 is 650 <br>");
+// var qii = (3*650);
+// document.write(sc+pi+qii);
+
+
+// var pi =("<br>price of item 2 is 100 <br>");
+// var qiii = (7*100);
+// document.write(pi+qiii);
+
+
+// var pi =("<br> shopping charges is");
+// var qiiii = (100);
+// document.write(pi+qiiii)
+
+
+// var to =("<br>total cost of your order is ");
+// var total=(700+1950+100); 
+// document.write( to+total);
+
+
+//===== chap #5 task7 =====
+// var totalMarks =("Total marks:");
+// var number =("980");
+// var totalMarks =("Total marks:");
+// var number =("804");
+// var per = ("percentage:")
+// var perc = 804*100/980;
+// document.write(per+perc)
+
+//===== chap #5 task8 =====
+// var currency = 104.80*10 + 28*25;
+// var pak = ("Total currency in PKR :")
+// document.write(pak+currency);
+
+//===== chap #5 task9 =====
+// var number = 5*10/2;
+// document.write(number);
+
+
+//===== chap #6to9 task1 =====
+// var a =(2);
+// var b =(1);
+// var c = --a - --b + ++b + b--;
+// alert(c);
+
+//===== chap #6to9 task2 =====
+// var userName = prompt("user Name");
+// var rollNumber = prompt("Roll Number");
+// var subject1 = Number (prompt("English Marks"));
+// var subject2 = Number (prompt("Urdu Marks"));
+// var subject3 = Number (prompt("Math Marks"));
+// var subject4 = Number (prompt("Java Marks"));
+// var totalMarks = subject1 + subject2 + subject3 + subject4;
+// var percentage = (totalMarks *  100) / 400;
+// alert(percentage);
+
+//===== chap #6to9 task3 =====
+// var userName = prompt("username")
+// var greet =(" wellcome")
+// alert (userName+greet);
+
+// var totalNum = prompt();
+//         console.log("Student Name", "===>    " + userName);
+//         console.log("Roll Number", "====>    " + rollNumber);
+//         console.log( "english ", "====>      " + subject1);
+//         console.log( "Urdu", "====>          " + subject2);
+//         console.log("Math", "====>           " + subject3);
+//         console.log("java", "====>           " + subject4);
+//         console.log("Total Makrs", "====>    " + totalMarks);
+//         console.log("percentage", "=====>    " + percentage );
+
+
+
+var name2 ="kjhkdkf";
+        var userName = prompt("user Name");
+        var rollNumber = prompt("Roll Number");
+        var subject1 = Number (prompt("English Marks"));
+        var subject2 = Number (prompt("Urdu Marks"));
+        var subject3 = Number (prompt("Math Marks"));
+        var subject4 = Number (prompt("Java Marks"));
+        var totalMarks = subject1 + subject2 + subject3 + subject4;
+        var percentage = (totalMarks *  100) / 400;
+
+
+
+        var totalNum = prompt()
+        document.write("Student Name", "         " + userName );
+        document.write(" <br>Roll Number", "      " + rollNumber);
+        document.write( "<br>english ", "     " + subject1);
+        document.write( "<br>Urdu", "         " + subject2);
+        document.write("<br>Math", "           " + subject3);
+        document.write("<br>java", "          " + subject4);
+        document.write("<br>Total Makrs", "    " + totalMarks);
+        document.write("<br>percentage", "   " + percentage );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
